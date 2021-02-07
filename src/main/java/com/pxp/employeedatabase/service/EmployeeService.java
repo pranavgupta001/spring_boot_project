1330c65d8cdbf1052c789c92a7933016200bb925
DELL <DELL@DESKTOP-96EVRAK>
2021-02-07 21:52:44 +0530

    First Commit

---
+++ src/main/java/com/pxp/employeedatabase/service/EmployeeService.java
@@ -0,0 +1,77 @@
+package com.pxp.employeedatabase.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pxp.employeedatabase.entity.EmployeeEntity;
+import com.pxp.employeedatabase.model.Employee;
+import com.pxp.employeedatabase.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+ @Autowired
+ EmployeeRepository employeeRepository;
+ public List<Employee> getAllEmployees(){
+	 try {
+		 List<EmployeeEntity> employees = employeeRepository.findAll();
+		 List<Employee>customEmployees = new ArrayList<>(); 	
+		 employees.stream().forEach(e ->{
+			 Employee employee = new Employee();
+			 BeanUtils.copyProperties(e, employee);
+			 customEmployees.add(employee);
+		 });
+		 return customEmployees;
+	 }
+	 catch(Exception e) {
+		 throw e;
+	 }
+ }
+ 
+ public String addEmployee(EmployeeEntity employee) {
+	 try {
+		 if(!employeeRepository.existbyfirstNameAndLastName(employee.getFirstName(), employee.getLastName())) {
+			 employeeRepository.save(employee);
+			 return "employee added succesfully";
+		 }
+		 else {
+			 return "this employee alreay exist";
+		 }
+	 }
+	 catch (Exception e) {
+		 throw e;
+	 }
+ }
+	 public String removeEmployee(EmployeeEntity employee) {
+		 try {
+			 if(employeeRepository.existbyfirstNameAndLastName(employee.getFirstName(), employee.getLastName())) {
+				 employeeRepository.delete(employee);
+				 return "Employee Delete";
+			 }
+			 else {
+				 return "Employee does not exist";
+			 }
+		 }
+		 catch(Exception e) {
+			 throw e;
+		 }
+	 }
+	 public String updateEmployee(EmployeeEntity employee) {
+		 try {
+			 if(employeeRepository.existsById(employee.getId())) {
+				 employeeRepository.save(employee);
+				 return "Employee Delete";
+			 }
+			 else {
+				 return "Employee does not exist";
+			 }
+		 }
+		 catch(Exception e) {
+			 throw e;
+		 }
+	 }
+ }
+
